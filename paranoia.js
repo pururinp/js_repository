77874e9b736f0ed8ee58a2b8295888f5a873ff39
Viktoriia Anpilohova <mvs.workst@gmail.com>
2025-04-15 16:00:15 +0300

    paranoia

---
+++ paranoia.js
@@ -0,0 +1,46 @@
+var arr = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com"
+    },
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        email:"dmitro.porohov@yahoo.com"
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru" 
+    },
+    {
+        userName:"Bad",
+        lastName:"Format",
+        email:"...@gmail.com" 
+    },
+    {
+        userName:"Good",
+        lastName:"Format",
+        email:"ll.cool4j@gmail.com"
+    },
+    {
+        userName:"alsoGood",
+        lastName:"Format",
+        email:"also.good@yahoo.com"
+    }
+];
+
+
+var trustedEmailRegex = /^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@(gmail\.com|yahoo\.com)$/;
+
+var trustedEmails = [];
+
+for (var i = 0; i < arr.length; i++) {
+    var email = arr[i].email;
+    if (trustedEmailRegex.test(email)) {
+        trustedEmails.push(email);
+    }
+}
+
+console.log(trustedEmails);
